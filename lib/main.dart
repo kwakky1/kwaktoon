aded7364bc923e07af3199988a58f591d68b2790
Andy Kwak <kwakky1@gmail.com>
2023-02-07 00:39:39 +0900

    pass state in flutter

--- lib/main.dart
+++ lib/main.dart
@@ -10,38 +10,43 @@ class App extends StatefulWidget {
 }
 
 class _AppState extends State<App> {
-  int counter = 0;
-
-  void onClicked() {
-    setState(() {
-      counter = counter + 1;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        textTheme: const TextTheme(
+          titleLarge: TextStyle(
+            color: Colors.red,
+          ),
+        ),
+      ),
       home: Scaffold(
         backgroundColor: const Color(0xFFF4EDDB),
         body: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const Text(
-                'Click Counter',
-                style: TextStyle(fontSize: 30),
-              ),
-              Text(
-                '$counter',
-                style: const TextStyle(fontSize: 28),
-              ),
-              IconButton(
-                onPressed: onClicked,
-                icon: const Icon(Icons.add),
-              )
+            children: const [
+              MyLargeTitle(),
             ],
           ),
         ),
+      ),
+    );
+  }
+}
+
+class MyLargeTitle extends StatelessWidget {
+  const MyLargeTitle({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      'My Large Title',
+      style: TextStyle(
+        fontSize: 30,
+        color: Theme.of(context).textTheme.titleLarge?.color,
       ),
     );
   }
